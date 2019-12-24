3dc6bfdd36cd7939ef5b1081c998f3ce0ad7b4ea
Tilo Hirsch <tilohirsch@Tilos-MacBook.local>
2019-12-24 06:38:39 +0100

    adding amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "ucom-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://nvoxvxfy5i.execute-api.eu-central-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_bSpWUJUB1",
+    APP_CLIENT_ID: "1j88s0aq128knrkggpsnr3lmvp",
+    IDENTITY_POOL_ID: "eu-central-1:9bf38d21-4f3f-49a7-9ddf-d9e8c2d21452"
+  }
+};
